5d3770d4d50e9fc00391f48be72f3446800d6a3d
nikwan <nikhil.courser@gmail.com>
2021-09-05 18:34:40 +0530

    added logs

--- src/com/gittest/test/Test2.java
+++ src/com/gittest/test/Test2.java
@@ -3,6 +3,7 @@ package com.gittest.test;
 public class Test2 {
 	
 	public static void main(String[] args) {
+		System.out.println("@@inside main");
 		System.out.println("added by bala wank....");
 	}
 
